2da46616c00dcfc38488a75b3a071a4d0fb1e560
ye4241 <ye4241@qq.com>
2019-06-15 11:13:42 +0800

    replace all raw source to ye4241;

--- ssr_old.sh
+++ ssr_old.sh
@@ -868,7 +868,7 @@ JQ_install(){
 	JQ_ver=`jq -V`
 	if [[ -z ${JQ_ver} ]]; then
 		#wget --no-check-certificate -N "https://softs.pw/Bash/other/jq-1.5.tar.gz"
-		wget --no-check-certificate -N "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/jq-1.5.tar.gz"
+		wget --no-check-certificate -N "https://raw.githubusercontent.com/ye4241/doubi/master/other/jq-1.5.tar.gz"
 		tar -xzf jq-1.5.tar.gz && cd jq-1.5
 		./configure --disable-maintainer-mode && make && make install
 		ldconfig
